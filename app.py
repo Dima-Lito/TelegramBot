fcc3c6aff9b7b8b7b34d1d1dea8b33ffff4f5019
Dima-Lito <101354825+Dima-Lito@users.noreply.github.com>
2023-05-20 15:24:13 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import telebot
 import requests
 import json
-from telebot import types   
+from telebot import types
+import datetime
+import os  
 
 
 bot = telebot.TeleBot('6033259761:AAGe25BeV-2QjckSa7BXxiTAVyvHCVDx8WE')
@@ -22,6 +24,9 @@ def help(message):
 def clear(message):
     bot.delete_message(message.chat.id, message.message_id)
 
+@bot.message_handler(commands=['about'])
+def about(message):
+    bot.send_message(message.chat.id,'Я з міста Сміла.')
 
 @bot.message_handler(commands=['Country', 'country' ,'Країни', 'країни'])
 def country(message):
@@ -36,8 +41,19 @@ def country(message):
     item8 = types.KeyboardButton('Germany')
     item9 = types.KeyboardButton('United Kingdom')
     item10 = types.KeyboardButton('Thailand')
+    item11 = types.KeyboardButton('Poland')
+    item12= types.KeyboardButton('Ireland')
+    item13 = types.KeyboardButton('Mexico')
+    item14 = types.KeyboardButton('Italy')
+    item15 = types.KeyboardButton('Netherlands')
+    item16 = types.KeyboardButton('Japan')
+    item17 = types.KeyboardButton('Philippines')
+    item18 = types.KeyboardButton('Croatia')
+    item19 = types.KeyboardButton('Hungary')
+    item20 = types.KeyboardButton('Austria')
+    
 
-    markup.add(item1, item2, item3, item4, item6, item7, item8, item9, item10,)
+    markup.add(item1, item2, item3, item4, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20)
 
     bot.send_message(message.chat.id, 'Вибери назву країни.'.format(message.from_user), reply_markup = markup)
 
@@ -45,6 +61,7 @@ def country(message):
 @bot.message_handler(commands=['UkraineCity', 'Ukrainecity', 'city'])
 def city(message):
      markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+     MyCity = types.KeyboardButton('Smila')
      item1 = types.KeyboardButton('Cherkasy')
      item2 = types.KeyboardButton('Kyiv')
      item3 = types.KeyboardButton('Uman')
@@ -70,7 +87,7 @@ def city(message):
      item24 = types.KeyboardButton('Kramatorsk')
      item25 = types.KeyboardButton('Kamianets-Podilskyi')
     
-     markup.add(item1, item2, item3, item4, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25)
+     markup.add(MyCity, item1, item2, item3, item4, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25)
 
      bot.send_message(message.chat.id, 'Вибери назву міста.'.format(message.from_user), reply_markup = markup)
 
@@ -83,18 +100,53 @@ def get_weather(message):
         data = json.loads(res.text)
         temp = data["main"]["temp"]
         feels_like = data["main"]["feels_like"]
-        bot.reply_to(message, f'Погода зараз: {temp}')
-        bot.reply_to(message, f'Відчувається як: {feels_like}')
+        city = data["name"]
+        humidity = data["main"]["humidity"]
+        wind = data["wind"]["speed"]
+        pressure = data["main"]["pressure"]
+        weather_description = data["weather"][0]["main"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
 
+        code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
+        #hotandcold = 'Жарко' if temp > 20.0 else 'Холодно'
+        weather_description = data["weather"][0]["main"]
 
-        image = 'sunny.png' if temp > 10.0 else 'sun.png'
-        file = open('./' + image, 'rb')
-        bot.send_photo(message.chat.id, file)
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            # если эмодзи для погоды нет, выводим другое сообщение
+            wd = "Посмотри в окно, я не понимаю, что там за погода..."
+
+        bot.reply_to(message, 
+        f"Погода в місті/країні: {city}\nТемпература: {temp}°C {wd}\n" 
+        f'Відчувається як: {feels_like}°C\n'
+        f"Вологість: {humidity}%\nВітер: {wind} м/с \n"
+        f"Схід сонця: {sunrise_timestamp}\nЗахід сонця: {sunset_timestamp}\nТривалість дня: {length_of_the_day}\n"
+        f"Гарного дня!"
+        )
+
+        #image = 'sunny.png' if temp > 10.0 else 'sun.png'
+        #file = open('./' + image, 'rb')
+        #bot.send_photo(message.chat.id, file)
     else:
         bot.reply_to(message, 'Місто указано неправильно!')
 
 @bot.message_handler(content_types=['photo', 'video'])
 def photo(message):
     bot.reply_to(message, 'Навіщо мені це?')
+
+@bot.message_handler(content_types=['audio'])
+def photo(message):
+    bot.reply_to(message, 'Навіщо мені музика?')
 
 bot.polling(none_stop=True)
